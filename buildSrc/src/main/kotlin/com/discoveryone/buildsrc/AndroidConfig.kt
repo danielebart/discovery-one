6efe4daafdc417b453cd15496179cd1efcb20245
Daniele Bartorilla <15895153+danielebart@users.noreply.github.com>
2020-04-17 14:51:42 +0200

    Revert "Decrease minSdkVersion to 19 (#9)"
    This reverts commit c5fb7140b04b406f14734835d9fded818728ddef.

--- buildSrc/src/main/kotlin/com/discoveryone/buildsrc/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/com/discoveryone/buildsrc/AndroidConfig.kt
@@ -3,7 +3,7 @@ package com.discoveryone.buildsrc
 object AndroidConfig {
 
     const val applicationId = "com.discoveryone"
-    const val minSdkVersion = 19
+    const val minSdkVersion = 21
     const val targetSdkVersion = 29
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
