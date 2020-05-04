3f25420d480506aedf21e624cc86890a56be6cdc
Daniele Bartorilla <15895153+danielebart@users.noreply.github.com>
2020-05-05 00:02:37 +0200

    add sample app (#43)

---
+++ sample/src/main/java/com/discoveryone/sample/foodmenu/FoodMenuHomeFragment.kt
@@ -0,0 +1,81 @@
+package com.discoveryone.sample.foodmenu
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.discoveryone.Scene
+import com.discoveryone.annotations.FragmentRoute
+import com.discoveryone.extensions.onResult
+import com.discoveryone.extensions.scene
+import com.discoveryone.sample.R
+import kotlinx.android.synthetic.main.fragment_foodmenu_home.*
+
+@FragmentRoute(name = "FoodMenuHome", containerId = R.id.foodMenuContainer)
+class FoodMenuHomeFragment : Fragment(R.layout.fragment_foodmenu_home), FoodMenuHomeView {
+
+    private val presenter = FoodMenuHomePresenter(scene, this)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        presenter.start()
+
+        launchMenuSelectionButton.setOnClickListener {
+            presenter.onLaunchMenuSelectionClick()
+        }
+
+        numberOfCustomersEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+                presenter.onNumberOfCustomersTextChange(s.toString())
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) =
+                Unit
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+
+        })
+    }
+
+    override fun showToast(text: String) {
+        Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show()
+    }
+}
+
+class FoodMenuHomePresenter(private val scene: Scene, private val view: FoodMenuHomeView) {
+
+    private var currentNumberOfCustomers = 0
+
+    fun start() {
+        scene.onResult<String>(SELECTION_RESULT_KEY) { foodOrder ->
+            view.showToast("Order completed for $currentNumberOfCustomers customers: $foodOrder")
+        }
+    }
+
+    fun onLaunchMenuSelectionClick() {
+        if (currentNumberOfCustomers > 0) {
+            scene.navigateForResult(
+                SELECTION_RESULT_KEY,
+                FoodMenuSelection(numberOfCustomers = currentNumberOfCustomers)
+            )
+        } else {
+            view.showToast("you must insert the number of customers first")
+        }
+    }
+
+    fun onNumberOfCustomersTextChange(newText: String) {
+        if (newText.toIntOrNull() != null) {
+            currentNumberOfCustomers = newText.toInt()
+        }
+    }
+
+    companion object {
+        private const val SELECTION_RESULT_KEY = "SELECTION_RESULT_KEY"
+    }
+}
+
+interface FoodMenuHomeView {
+
+    fun showToast(text: String)
+}
---
+++ sample/src/main/java/com/discoveryone/sample/foodmenu/FoodMenuSelectionFragment.kt
@@ -0,0 +1,38 @@
+package com.discoveryone.sample.foodmenu
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.discoveryone.annotations.FragmentRoute
+import com.discoveryone.annotations.RouteArgument
+import com.discoveryone.extensions.scene
+import com.discoveryone.sample.R
+import kotlinx.android.synthetic.main.fragment_foodmenu_selection.*
+
+@FragmentRoute(
+    name = "FoodMenuSelection",
+    containerId = R.id.foodMenuContainer,
+    arguments = [RouteArgument("numberOfCustomers", Int::class)]
+)
+class FoodMenuSelectionFragment : Fragment(R.layout.fragment_foodmenu_selection) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        numberOfCustomersTextView.text = getString(
+            R.string.number_of_customers,
+            requireArguments().getInt("numberOfCustomers")
+        )
+
+        cheeseburgerButton.setOnClickListener {
+            scene.closeWithResult(cheeseburgerButton.text)
+        }
+
+        sushiButton.setOnClickListener {
+            scene.closeWithResult(sushiButton.text)
+        }
+
+        pizzaButton.setOnClickListener {
+            scene.closeWithResult(pizzaButton.text)
+        }
+    }
+}
